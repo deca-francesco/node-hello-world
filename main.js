bcb8c5b508610a62a1d6e30fa96761bcae2d5143
deca-francesco <francescodecarlo70@gmail.com>
2024-10-28 16:41:33 +0100

    ADD: env and argv variables

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("Hello node js");
+console.log(process.argv);
+
+// variabile d'ambiente. STESSO NOME nella dichiarazione e nella proprietà.
+const pinEnv = process.env.pinEnv;
+console.log(pinEnv);
+
+// argomento dello script: tutti gli elementi dell'array process.argv a partire dall'indice 2.
+const pinArg = process.argv.slice(2);
+console.log(pinArg);
